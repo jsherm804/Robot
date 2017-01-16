801e78ad53d7a250169276a96d5c0b023fceb931
Dylan McAllister <dylan@seawaterfox.com>
2017-01-16 16:52:39 -0500

    Initial Commit

---
+++ Steamworks Robot - Tommy/src/org/usfirst/frc/team6627/robot/Robot.java
@@ -0,0 +1,79 @@
+package org.usfirst.frc.team6627.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	RobotDrive SWRobot = new RobotDrive(0, 1);
+	Joystick mainStick = new Joystick(0); // Main drive stick
+	Joystick secStick = new Joystick(1); // Likely to be pneumatics
+	Timer timer = new Timer();
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		CameraServer.getInstance().startAutomaticCapture();
+	}
+
+	/**
+	 * This function is run once each time the robot enters autonomous mode
+	 */
+	@Override
+	public void autonomousInit() {
+		timer.reset();
+		timer.start();
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		// Drive for 2 seconds
+		if (timer.get() < 2.0) {
+			SWRobot.drive(0.5, 0.0); // drive forwards half speed
+		} else if (timer.get() < 5.0) {
+			SWRobot.drive(-0.5, 0.0); // stop robot
+		} else {
+			SWRobot.drive(0.0,  0.0);
+		}
+	}
+
+	/**
+	 * This function is called once each time the robot enters tele-operated
+	 * mode
+	 */
+	@Override
+	public void teleopInit() {
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		SWRobot.arcadeDrive(mainStick);
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+}
